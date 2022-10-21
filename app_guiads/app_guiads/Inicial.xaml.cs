4c3b2a3cf21f05be98fc06ca48e9117accdcbb29
marianepagagnotti <marianepagagnotti.sl@gmail.com>
2022-10-21 20:33:15 -0300

    adicionando carrossel e alterações

--- app_guiads/app_guiads/Inicial.xaml.cs
+++ app_guiads/app_guiads/Inicial.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,34 @@ namespace app_guiads
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Inicial : ContentPage
     {
-        public Inicial()
+
+        public class ImageInformation
+        {
+            public ImageSource _Image { get; set; }
+        }
+        private ObservableCollection<ImageInformation> imageCollection;
+
+        public ObservableCollection<ImageInformation> ImageCollection
+        {
+            get { return imageCollection; }
+            set
+            {
+                imageCollection = value;
+                OnPropertyChanged();
+            }
+        }
+        
+            public Inicial()
         {
             InitializeComponent();
 
-            
+            BindingContext = this;
+            ImageCollection = new ObservableCollection<ImageInformation>
+            {
+                new ImageInformation{ _Image ="foto1.jpg" },
+                new ImageInformation{ _Image ="foto2.jpg" },
+                new ImageInformation{ _Image ="foto3.jpg" }
+            };
         }
     }
 }
